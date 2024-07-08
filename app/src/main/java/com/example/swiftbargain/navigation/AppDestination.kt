82269989585d8507b3f7e5e3c43b9d309832f9f0
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-08 18:11:08 +0300

    add app destination

---
+++ app/src/main/java/com/example/swiftbargain/navigation/AppDestination.kt
@@ -0,0 +1,19 @@
+package com.example.swiftbargain.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Authentication
+
+@Serializable
+object Login
+
+@Serializable
+object Register
+
+@Serializable
+object User
+
+@Serializable
+object Home
+
