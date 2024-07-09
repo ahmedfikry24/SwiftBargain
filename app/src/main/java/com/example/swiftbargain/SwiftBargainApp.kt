ae8d27a372443b540ec215ce214e8e8ee8fecf84
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-09 12:06:08 +0300

    separate auth & user navGraphs

--- app/src/main/java/com/example/swiftbargain/SwiftBargainApp.kt
+++ app/src/main/java/com/example/swiftbargain/SwiftBargainApp.kt
@@ -3,30 +3,17 @@ package com.example.swiftbargain
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.swiftbargain.navigation.Authentication
-import com.example.swiftbargain.navigation.Home
-import com.example.swiftbargain.navigation.Login
-import com.example.swiftbargain.navigation.Register
+import com.example.swiftbargain.navigation.AuthenticationNavGraph
 import com.example.swiftbargain.navigation.User
+import com.example.swiftbargain.navigation.UserNavGraph
 
 @Composable
 fun SwiftBargainApp() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Authentication) {
-        navigation<Authentication>(startDestination = Login) {
-            composable<Login> {
-
-            }
-            composable<Register> {
-
-            }
-        }
-        navigation<User>(startDestination = Home) {
-            composable<Home> {
-
-            }
-        }
+        composable<Authentication> { AuthenticationNavGraph(mainNavController = navController) }
+        composable<User> { UserNavGraph(mainNavController = navController) }
     }
 }
