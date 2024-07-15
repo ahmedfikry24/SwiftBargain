8790d16f68b24ce2d6508296f44f54960bb120ea
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-15 17:34:35 +0300

    add UiConstants

---
+++ app/src/main/java/com/example/swiftbargain/ui/utils/UiConstants.kt
@@ -0,0 +1,7 @@
+package com.example.swiftbargain.ui.utils
+
+object UiConstants {
+    const val INVALID_AUTH = "invalid email or password"
+    const val NO_INTER_NET_CONNECTION = "no internet connection"
+    const val SOME_THING_WENT_WRONG = "something went wrong, check internet then try again"
+}
