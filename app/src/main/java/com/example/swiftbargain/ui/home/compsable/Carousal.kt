d535648e8587c6d8d88bfccbd7c06a583bba4f04
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-13 23:48:17 +0300

    add banner composable

--- app/src/main/java/com/example/swiftbargain/ui/home/compsable/Carousal.kt
+++ app/src/main/java/com/example/swiftbargain/ui/home/compsable/Carousal.kt
@@ -6,24 +6,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import com.example.swiftbargain.ui.composable.Banner
 import com.example.swiftbargain.ui.theme.colors
 import com.example.swiftbargain.ui.theme.spacing
 import com.example.swiftbargain.ui.utils.shared_ui_state.SaleAdUiState
@@ -45,29 +39,10 @@ fun Carousal(
             state = pagerState,
             reverseLayout = true
         ) { page ->
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(MaterialTheme.spacing.space4)),
-                contentAlignment = Alignment.Center
-            ) {
-                AsyncImage(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(180.dp),
-                    model = items[page].url,
-                    contentDescription = null,
-                    contentScale = ContentScale.FillBounds
-                )
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(MaterialTheme.spacing.space16),
-                    text = items[page].title,
-                    style = MaterialTheme.typography.headlineLarge,
-                    color = MaterialTheme.colors.background
-                )
-            }
+            Banner(
+                url = items[page].url,
+                title = items[page].title
+            )
         }
         CarouselIndicator(pagerState = pagerState, listSize = items.size)
     }
