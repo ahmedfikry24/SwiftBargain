d5b5b48bf9fb4f34355c82d2967afcbbc621edbb
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-13 21:08:15 +0300

    implement composables into home screen

--- app/src/main/java/com/example/swiftbargain/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/swiftbargain/ui/home/HomeScreen.kt
@@ -1,19 +1,30 @@
 package com.example.swiftbargain.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.composable.ContentLoading
+import com.example.swiftbargain.ui.composable.ContentVisibility
+import com.example.swiftbargain.ui.composable.SearchBar
+import com.example.swiftbargain.ui.home.compsable.Carousal
+import com.example.swiftbargain.ui.home.compsable.HomeCategories
+import com.example.swiftbargain.ui.home.compsable.HomeSaleSection
 import com.example.swiftbargain.ui.home.view_model.HomeInteractions
 import com.example.swiftbargain.ui.home.view_model.HomeUiState
 import com.example.swiftbargain.ui.home.view_model.HomeViewModel
 import com.example.swiftbargain.ui.theme.spacing
+import com.example.swiftbargain.ui.utils.ContentStatus
 import com.example.swiftbargain.ui.utils.EventHandler
 
 @Composable
@@ -36,10 +47,48 @@ private fun HomeContent(
     state: HomeUiState,
     interactions: HomeInteractions
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(MaterialTheme.spacing.space16)
-    ) {
-
+    ContentLoading(isVisible = state.contentStatus == ContentStatus.LOADING)
+    ContentVisibility(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(MaterialTheme.spacing.space16),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        ) {
+            item {
+                SearchBar(
+                    value = state.search,
+                    onClickKeyboardDone = { },
+                    onChangeValue = {},
+                    onClickFavourite = { },
+                    onClickNotification = {}
+                )
+            }
+            item {
+                Carousal(items = state.saleAds)
+            }
+            item {
+                if (state.categories.isNotEmpty())
+                    HomeCategories(
+                        modifier = Modifier.padding(top = MaterialTheme.spacing.space8),
+                        categories = state.categories
+                    )
+            }
+            item {
+                if (state.flashSale.isNotEmpty())
+                    HomeSaleSection(
+                        modifier = Modifier.padding(top = MaterialTheme.spacing.space8),
+                        sectionName = stringResource(R.string.flash_sale),
+                        items = state.flashSale
+                    )
+            }
+            item {
+                if (state.megaSale.isNotEmpty())
+                    HomeSaleSection(
+                        modifier = Modifier.padding(top = MaterialTheme.spacing.space8),
+                        sectionName = stringResource(R.string.mega_sale),
+                        items = state.megaSale
+                    )
+            }
+        }
     }
 }
