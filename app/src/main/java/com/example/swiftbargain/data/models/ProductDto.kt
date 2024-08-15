9d4f1ccaaf6dbe359b73f4b38ac377d5fc3fb3fb
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-15 23:55:47 +0300

    update ProductDto && SaleAdDto with property

--- app/src/main/java/com/example/swiftbargain/data/models/ProductDto.kt
+++ app/src/main/java/com/example/swiftbargain/data/models/ProductDto.kt
@@ -6,16 +6,24 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ProductDto(
     val id: String = "",
+
     @get:PropertyName("category_id")
     @set:PropertyName("category_id")
     var categoryId: String = "",
+
     val title: String = "",
     val description: String = "",
     val price: String = "",
     val recommended: Boolean = false,
+
     @get:PropertyName("discount_percentage")
     @set:PropertyName("discount_percentage")
     var discountPercentage: String = "",
+
+    @get:PropertyName("sale_id")
+    @set:PropertyName("sale_id")
+    var saleId: String = "",
+
     val rate: String = "",
     val url: List<String> = listOf()
 )
