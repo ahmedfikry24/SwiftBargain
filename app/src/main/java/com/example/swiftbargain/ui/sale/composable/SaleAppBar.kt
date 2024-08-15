43a6dcde0a16caaa60154541d2a1b331e233a9a1
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-15 23:56:45 +0300

    add SaleAppBar composable

---
+++ app/src/main/java/com/example/swiftbargain/ui/sale/composable/SaleAppBar.kt
@@ -0,0 +1,67 @@
+package com.example.swiftbargain.ui.sale.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.theme.colors
+import com.example.swiftbargain.ui.theme.spacing
+
+@Composable
+fun SaleAppBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClickBack: () -> Unit,
+    onClickSearch: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space12)
+        ) {
+            IconButton(
+                colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colors.textGrey),
+                onClick = onClickBack
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_bcak),
+                    contentDescription = null
+                )
+            }
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colors.text
+            )
+        }
+
+        IconButton(
+            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colors.textGrey),
+            onClick = onClickSearch
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_search),
+                contentDescription = null
+            )
+        }
+    }
+}
+
