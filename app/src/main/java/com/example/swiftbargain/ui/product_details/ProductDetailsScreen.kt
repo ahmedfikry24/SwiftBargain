cd8c2aa9c46077e988cc7051b1f1304a2cb531b1
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-25 23:50:48 +0300

    design half ProductDetailsScreen

--- app/src/main/java/com/example/swiftbargain/ui/product_details/ProductDetailsScreen.kt
+++ app/src/main/java/com/example/swiftbargain/ui/product_details/ProductDetailsScreen.kt
@@ -1,16 +1,35 @@
 package com.example.swiftbargain.ui.product_details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.composable.ContentError
+import com.example.swiftbargain.ui.composable.ContentLoading
+import com.example.swiftbargain.ui.composable.ContentVisibility
+import com.example.swiftbargain.ui.composable.PrimaryAppbar
+import com.example.swiftbargain.ui.product_details.composable.DetailsCarousal
+import com.example.swiftbargain.ui.product_details.composable.DetailsColors
+import com.example.swiftbargain.ui.product_details.composable.DetailsSizes
+import com.example.swiftbargain.ui.product_details.composable.DetailsTitle
 import com.example.swiftbargain.ui.product_details.view_model.ProductDetailsInteractions
 import com.example.swiftbargain.ui.product_details.view_model.ProductDetailsUiState
 import com.example.swiftbargain.ui.product_details.view_model.ProductDetailsViewModel
+import com.example.swiftbargain.ui.theme.colors
+import com.example.swiftbargain.ui.theme.spacing
+import com.example.swiftbargain.ui.utils.ContentStatus
+import com.example.swiftbargain.ui.utils.EventHandler
 
 
 @Composable
@@ -19,7 +38,12 @@ fun ProductDetailsScreen(
     viewModel: ProductDetailsViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    EventHandler(effects = viewModel.event) { events, _ ->
+        when (events) {
 
+            else -> Unit
+        }
+    }
     ProductDetailsContent(state = state, interactions = viewModel)
 }
 
@@ -28,9 +52,34 @@ private fun ProductDetailsContent(
     state: ProductDetailsUiState,
     interactions: ProductDetailsInteractions
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ) {
-
+    ContentLoading(isVisible = state.contentStatus == ContentStatus.LOADING)
+    ContentVisibility(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        ) {
+            item { PrimaryAppbar(title = stringResource(R.string.details), onClickBack = {}) }
+            item { DetailsCarousal(imagesUrl = state.product.url) }
+            item {
+                DetailsTitle(
+                    title = state.product.title,
+                    isFavorite = state.isFavorite,
+                    rate = state.product.rate.toFloat(),
+                    onCLickFavorite = {}
+                )
+            }
+            item {
+                Text(
+                    modifier = Modifier.padding(start = MaterialTheme.spacing.space16),
+                    text = "$ ${state.product.priceAfterDiscount}",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+            item { DetailsSizes(state = state, onClickSize = {}) }
+            item { DetailsColors(state = state, onClickColor = {}) }
+        }
     }
+    ContentError(isVisible = state.contentStatus == ContentStatus.FAILURE, onTryAgain = {})
 }
