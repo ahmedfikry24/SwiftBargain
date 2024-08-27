ab004fc9ce85273d82dc87ddc1819409623a42cd
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-27 23:53:15 +0300

    add ReviewsScreen && navType

---
+++ app/src/main/java/com/example/swiftbargain/ui/reviews/ReviewsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.swiftbargain.ui.reviews
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ReviewsScreen(navController: NavController) {
+
+}
