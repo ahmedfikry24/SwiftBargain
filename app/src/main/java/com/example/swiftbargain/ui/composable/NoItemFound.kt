902eaa45e2ca3d36b2c9adeb5890f386421bd68b
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-29 17:57:05 +0300

    refactor empty list

--- app/src/main/java/com/example/swiftbargain/ui/composable/NoItemFound.kt
+++ app/src/main/java/com/example/swiftbargain/ui/composable/NoItemFound.kt
@@ -1,17 +1,17 @@
 package com.example.swiftbargain.ui.composable
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
@@ -20,18 +20,19 @@ import com.example.swiftbargain.R
 @Composable
 fun NoItemFound(
     modifier: Modifier = Modifier,
-    isVisible: Boolean
+    isVisible: Boolean,
+    enterTransition: EnterTransition = fadeIn(tween(500)) + scaleIn(tween(500)),
+    exitTransition: ExitTransition = fadeOut(tween(500)) + scaleOut(tween(500))
 ) {
-    val size = (LocalConfiguration.current.screenWidthDp / 2).dp
     AnimatedVisibility(
         modifier = modifier,
         visible = isVisible,
-        enter = fadeIn(tween(500)) + scaleIn(tween(500)),
-        exit = fadeOut(tween(500)) + scaleOut(tween(500)),
+        enter = enterTransition,
+        exit = exitTransition,
     ) {
         val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_no_item_found))
         LottieAnimation(
-            modifier = Modifier.size(size),
+            modifier = Modifier.fillMaxSize(),
             composition = composition
         )
     }
