76a551b32ce9959b009096da059c977c9ca9d020
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-01 15:13:47 +0300

    create explore screen

---
+++ app/src/main/java/com/example/swiftbargain/ui/explore/ExploreScreen.kt
@@ -0,0 +1,16 @@
+package com.example.swiftbargain.ui.explore
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ExploreScreen(
+    navController: NavController
+) {
+    ExploreContent()
+}
+
+@Composable
+private fun ExploreContent() {
+
+}
