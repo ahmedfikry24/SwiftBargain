053d7aa79d0b33a82a5d276770478b564c2a2345
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-02 23:05:53 +0300

    implement require logic in VM

--- app/src/main/java/com/example/swiftbargain/ui/explore/view_model/ExploreInteractions.kt
+++ app/src/main/java/com/example/swiftbargain/ui/explore/view_model/ExploreInteractions.kt
@@ -1,3 +1,11 @@
 package com.example.swiftbargain.ui.explore.view_model
 
-interface ExploreInteractions
+interface ExploreInteractions {
+    fun getCategories()
+    fun onClickCategory(id: String, label: String)
+    fun controlSearchVisibility()
+    fun onChangeSearch(text: String)
+    fun getSearchResult()
+    fun onClickMic()
+    fun onClickProduct(id: String)
+}
