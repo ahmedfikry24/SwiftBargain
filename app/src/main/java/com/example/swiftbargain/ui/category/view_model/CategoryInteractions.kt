544aa90fe2e979d817b310c03face37fbd2c7e39
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-04 15:47:42 +0300

    add require events & interactions

--- app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryInteractions.kt
+++ app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryInteractions.kt
@@ -1,3 +1,8 @@
 package com.example.swiftbargain.ui.category.view_model
 
-interface CategoryInteractions
+interface CategoryInteractions {
+    fun getProducts()
+    fun onCLickBack()
+    fun onClickProduct(id: String)
+    fun getMoreProducts(id: String?)
+}
