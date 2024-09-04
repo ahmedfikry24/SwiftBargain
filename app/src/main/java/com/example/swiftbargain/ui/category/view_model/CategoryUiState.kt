87a50f8fbca50fe0129715ed0166da38df7456a2
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-04 15:48:27 +0300

    add require viewModel logic

--- app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryUiState.kt
+++ app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryUiState.kt
@@ -1,7 +1,12 @@
 package com.example.swiftbargain.ui.category.view_model
 
 import com.example.swiftbargain.ui.utils.ContentStatus
+import com.example.swiftbargain.ui.utils.shared_ui_state.ProductUiState
 
 data class CategoryUiState(
-    val contentStatus: ContentStatus = ContentStatus.VISIBLE
+    val contentStatus: ContentStatus = ContentStatus.LOADING,
+    val title: String = "",
+    val products: List<ProductUiState> = listOf(),
+    val isLoadMoreProducts: Boolean = false,
+    val pageNumber: Int = 1
 ) 
--- app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryViewModel.kt
+++ app/src/main/java/com/example/swiftbargain/ui/category/view_model/CategoryViewModel.kt
@@ -1,9 +1,78 @@
 package com.example.swiftbargain.ui.category.view_model
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.toRoute
+import com.example.swiftbargain.data.models.ProductDto
+import com.example.swiftbargain.data.repository.Repository
+import com.example.swiftbargain.navigation.Category
 import com.example.swiftbargain.ui.base.BaseViewModel
+import com.example.swiftbargain.ui.utils.ContentStatus
+import com.example.swiftbargain.ui.utils.shared_ui_state.toUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryViewModel @Inject constructor() :
-    BaseViewModel<CategoryUiState, CategoryEvents>(CategoryUiState()), CategoryInteractions
+class CategoryViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val repository: Repository
+) : BaseViewModel<CategoryUiState, CategoryEvents>(CategoryUiState()), CategoryInteractions {
+    private val args = savedStateHandle.toRoute<Category>()
+
+    init {
+        getProducts()
+    }
+
+
+    override fun getProducts() {
+        tryExecute(
+            { repository.getCategoryProducts(args.id, null) },
+            ::productsSuccess,
+            { productsError() }
+        )
+    }
+
+    private fun productsSuccess(products: List<ProductDto>) {
+        _state.update { value ->
+            value.copy(
+                contentStatus = ContentStatus.VISIBLE,
+                title = args.label,
+                products = products.map { it.toUiState() }
+            )
+        }
+    }
+
+    override fun getMoreProducts(id: String?) {
+        _state.update { it.copy(isLoadMoreProducts = true) }
+        tryExecute(
+            { repository.getSaleProducts(args.id, id) },
+            ::moreProductsSuccess,
+            { productsError() }
+        )
+    }
+
+    private fun moreProductsSuccess(products: List<ProductDto>) {
+        _state.update { value ->
+            value.copy(
+                isLoadMoreProducts = false,
+                products = value.products.toMutableList().apply {
+                    addAll(products.map { it.toUiState() })
+                },
+                pageNumber = value.pageNumber.inc(),
+            )
+        }
+    }
+
+    private fun productsError() {
+        _state.update { it.copy(contentStatus = ContentStatus.FAILURE, isLoadMoreProducts = false) }
+    }
+
+    override fun onCLickBack() {
+        sendEvent(CategoryEvents.NavigateToBack)
+    }
+
+    override fun onClickProduct(id: String) {
+        sendEvent(CategoryEvents.NavigateToProductDetails(id))
+    }
+
+}
