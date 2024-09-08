70e62120b4a7f265201fad4ceba9883d8ef04bb1
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-08 20:28:51 +0300

    handle add to cart check

--- app/src/main/java/com/example/swiftbargain/ui/utils/UiConstants.kt
+++ app/src/main/java/com/example/swiftbargain/ui/utils/UiConstants.kt
@@ -8,6 +8,7 @@ object UiConstants {
     const val SOME_THING_WENT_WRONG = "something went wrong, check internet then try again"
     const val REGISTRATION_FAILED = "registration failed, try to register again"
     const val EMAIL_ALREADY_USED = "this email address is already used by another account"
+    const val COMPLETE_CART_INFO = "please select oder info"
     const val ADD_TO_CART_SUCCESS = "add to cart successfully"
     const val ADD_REVIEW_SUCCESS = "review added successfully"
 }
