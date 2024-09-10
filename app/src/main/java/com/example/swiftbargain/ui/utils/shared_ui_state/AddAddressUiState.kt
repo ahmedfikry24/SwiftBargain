28261ef90f5c4e20f8653e8d24638bb5ab119c46
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-10 22:27:59 +0300

    add shared AddAddress uiState & interactions

---
+++ app/src/main/java/com/example/swiftbargain/ui/utils/shared_ui_state/AddAddressUiState.kt
@@ -0,0 +1,19 @@
+package com.example.swiftbargain.ui.utils.shared_ui_state
+
+data class AddAddressUiState(
+    val firstName: String = "",
+    val firstNameError: Boolean = false,
+    val lastname: String = "",
+    val lastnameError: Boolean = false,
+    val streetAddress: String = "",
+    val streetAddressError: Boolean = false,
+    val streetAddress2: String = "",
+    val city: String = "",
+    val cityError: Boolean = false,
+    val region: String = "",
+    val regionError: Boolean = false,
+    val zipCode: String = "",
+    val zipCodeError: Boolean = false,
+    val phone: String = "",
+    val phoneError: Boolean = false,
+)
---
+++ app/src/main/java/com/example/swiftbargain/ui/utils/shred_interactions/AddAddressInteractions.kt
@@ -0,0 +1,3 @@
+package com.example.swiftbargain.ui.utils.shred_interactions
+
+interface AddAddressInteractions
