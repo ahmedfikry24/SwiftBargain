72a419bb9a0e8c1adfe45234d5c2d40b6572f828
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-10 14:26:38 +0300

    design: rest of screen

--- app/src/main/java/com/example/swiftbargain/ui/cart/CartScreen.kt
+++ app/src/main/java/com/example/swiftbargain/ui/cart/CartScreen.kt
@@ -2,16 +2,25 @@ package com.example.swiftbargain.ui.cart
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.cart.composable.CartDetails
 import com.example.swiftbargain.ui.cart.composable.CartProduct
 import com.example.swiftbargain.ui.cart.view_model.CartInteractions
 import com.example.swiftbargain.ui.cart.view_model.CartUiState
@@ -19,6 +28,9 @@ import com.example.swiftbargain.ui.cart.view_model.CartViewModel
 import com.example.swiftbargain.ui.composable.ContentError
 import com.example.swiftbargain.ui.composable.ContentLoading
 import com.example.swiftbargain.ui.composable.ContentVisibility
+import com.example.swiftbargain.ui.composable.NoItemFound
+import com.example.swiftbargain.ui.composable.PrimaryTextButton
+import com.example.swiftbargain.ui.composable.PrimaryTextField
 import com.example.swiftbargain.ui.theme.spacing
 import com.example.swiftbargain.ui.utils.ContentStatus
 import com.example.swiftbargain.ui.utils.EventHandler
@@ -56,10 +68,50 @@ fun CartContent(
                     onClickItem = interactions::onClickItem
                 )
             }
+            item {
+                NoItemFound(
+                    modifier = Modifier.fillMaxSize(),
+                    isVisible = state.products.isEmpty()
+                )
+            }
+            item {
+                if (state.products.isNotEmpty())
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = MaterialTheme.spacing.space16)
+                            .heightIn(max = 56.dp)
+                    ) {
+                        PrimaryTextField(
+                            modifier = Modifier.weight(1f),
+                            value = state.couponCode,
+                            hint = stringResource(R.string.enter_coupon_code),
+                            isError = state.couponCodeError,
+                            onChangeValue = interactions::onChangeCouponCode
+                        )
+                        PrimaryTextButton(
+                            modifier = Modifier.fillMaxHeight(),
+                            text = stringResource(R.string.apply),
+                            onClick = interactions::checkCouponCode
+                        )
+                    }
+            }
+
+            item {
+                if (state.products.isNotEmpty())
+                    CartDetails(state = state)
+            }
+            item {
+                PrimaryTextButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(R.string.check_out),
+                    onClick = {}
+                )
+            }
         }
     }
     ContentError(
         isVisible = state.contentStatus == ContentStatus.FAILURE,
-        onTryAgain = {}
+        onTryAgain = interactions::getDate
     )
 }
