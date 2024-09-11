1b7801f81274b9c5ce4d2d6270bf1e14ab170eb5
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-11 21:22:28 +0300

    design AddressItem

--- app/src/main/java/com/example/swiftbargain/ui/cart_check_out/composable/ShipToContent.kt
+++ app/src/main/java/com/example/swiftbargain/ui/cart_check_out/composable/ShipToContent.kt
@@ -1,8 +1,11 @@
 package com.example.swiftbargain.ui.cart_check_out.composable
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -10,6 +13,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.swiftbargain.ui.cart_check_out.view_model.CartCheckOutInteractions
 import com.example.swiftbargain.ui.cart_check_out.view_model.CartCheckOutUiState
+import com.example.swiftbargain.ui.composable.AddressItem
 import com.example.swiftbargain.ui.composable.ScrollToFirstItemFab
 import com.example.swiftbargain.ui.theme.spacing
 import kotlinx.coroutines.launch
@@ -30,12 +34,23 @@ fun ShipToContent(
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             state = scrollState,
-            contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16)
+            contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
         ) {
             item {
                 ShipToAppbar(
                     onClickBack = interactions::onClickBack,
                     onClickAddAddress = interactions::controlAddAddressVisibility
+                )
+            }
+            items(state.allAddresses) { address ->
+                AddressItem(
+                    modifier = Modifier.padding(horizontal = MaterialTheme.spacing.space16),
+                    isSelected = state.selectedAddress == address,
+                    address = address,
+                    onClickItem = { interactions.onSelectAddress(address) },
+                    onClickEdit = interactions::onEditAddress,
+                    onClickDelete = interactions::onDeleteAddress
                 )
             }
         }
---
+++ app/src/main/java/com/example/swiftbargain/ui/composable/AddressItem.kt
@@ -0,0 +1,88 @@
+package com.example.swiftbargain.ui.composable
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.theme.colors
+import com.example.swiftbargain.ui.theme.spacing
+import com.example.swiftbargain.ui.utils.shared_ui_state.AddAddressUiState
+
+@Composable
+fun AddressItem(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    address: AddAddressUiState,
+    onClickItem: () -> Unit,
+    onClickEdit: () -> Unit,
+    onClickDelete: () -> Unit
+) {
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.background,
+        shape = RoundedCornerShape(MaterialTheme.spacing.space4),
+        border = BorderStroke(
+            1.dp,
+            if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.textLight
+        ),
+        onClick = onClickItem
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(MaterialTheme.spacing.space16),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        ) {
+            Text(
+                text = address.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colors.text
+            )
+            Text(
+                text = "${address.country}/${address.city}/${address.streetAddress}, ${address.streetAddress2} ${address.region}",
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colors.textGrey
+            )
+            Text(
+                text = address.phone,
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colors.textGrey
+            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                PrimaryTextButton(
+                    text = stringResource(R.string.edit),
+                    onClick = onClickEdit
+                )
+                IconButton(
+                    modifier = Modifier.padding(start = MaterialTheme.spacing.space8),
+                    onClick = onClickDelete,
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = MaterialTheme.colors.background,
+                        contentColor = MaterialTheme.colors.textGrey
+                    )
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_trash),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
+}
