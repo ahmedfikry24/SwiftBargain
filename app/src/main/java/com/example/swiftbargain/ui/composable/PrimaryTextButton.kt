c6495027d0ea413607044e58b400fa303fffbaea
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-15 16:13:38 +0300

    update: PrimaryTextButton

--- app/src/main/java/com/example/swiftbargain/ui/composable/PrimaryTextButton.kt
+++ app/src/main/java/com/example/swiftbargain/ui/composable/PrimaryTextButton.kt
@@ -20,17 +20,21 @@ fun PrimaryTextButton(
     containerColor: Color = MaterialTheme.colors.primary,
     contentColor: Color = MaterialTheme.colors.background,
     border: BorderStroke? = null,
+    isEnabled: Boolean = true,
     onClick: () -> Unit
 ) {
     TextButton(
         modifier = modifier,
         colors = ButtonDefaults.buttonColors(
             containerColor = containerColor,
-            contentColor = contentColor
+            contentColor = contentColor,
+            disabledContainerColor = MaterialTheme.colors.textLight,
+            disabledContentColor = MaterialTheme.colors.textGrey
         ),
         shape = RoundedCornerShape(MaterialTheme.spacing.space4),
         contentPadding = PaddingValues(MaterialTheme.spacing.space16),
         border = border,
+        enabled = isEnabled,
         onClick = onClick
     ) {
         Text(
