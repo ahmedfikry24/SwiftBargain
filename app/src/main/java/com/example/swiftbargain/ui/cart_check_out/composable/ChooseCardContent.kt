1b13a4ebd9dab57ba068f84beb361ef7e28e2ed2
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-16 22:20:20 +0300

    handle contentStatus wi ChooseCard

--- app/src/main/java/com/example/swiftbargain/ui/cart_check_out/composable/ChooseCardContent.kt
+++ app/src/main/java/com/example/swiftbargain/ui/cart_check_out/composable/ChooseCardContent.kt
@@ -17,11 +17,15 @@ import androidx.compose.ui.res.stringResource
 import com.example.swiftbargain.R
 import com.example.swiftbargain.ui.cart_check_out.view_model.CartCheckOutInteractions
 import com.example.swiftbargain.ui.cart_check_out.view_model.CartCheckOutUiState
+import com.example.swiftbargain.ui.composable.ContentError
+import com.example.swiftbargain.ui.composable.ContentLoading
+import com.example.swiftbargain.ui.composable.ContentVisibility
 import com.example.swiftbargain.ui.composable.CreditCardItem
 import com.example.swiftbargain.ui.composable.PrimaryTextButton
 import com.example.swiftbargain.ui.composable.ScrollToFirstItemFab
 import com.example.swiftbargain.ui.theme.colors
 import com.example.swiftbargain.ui.theme.spacing
+import com.example.swiftbargain.ui.utils.ContentStatus
 import com.example.swiftbargain.ui.utils.shared_ui_state.CreditUiSate
 import kotlinx.coroutines.launch
 
@@ -33,45 +37,53 @@ fun ChooseCardContent(
 ) {
     val scrollState = rememberLazyListState()
     val scope = rememberCoroutineScope()
-    ScrollToFirstItemFab(
-        modifier = modifier.fillMaxSize(),
-        isFabVisible = scrollState.canScrollBackward,
-        onClickFab = { scope.launch { scrollState.animateScrollToItem(0) } }
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(
-                modifier = Modifier.weight(1f),
-                state = scrollState,
-                contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
-                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
-            ) {
-                item {
-                    AddItemAppbar(
-                        title = stringResource(R.string.choose_card),
-                        onClickBack = { interactions.onSwitchContent(CartCheckOutUiState.VisibleContent.PAYMENT) },
-                        onClickAdd = interactions::controlAddCreditVisibility
-                    )
-                }
+    ContentLoading(isVisible = state.chooseCardContentStatus == ContentStatus.LOADING)
+    ContentVisibility(isVisible = state.chooseCardContentStatus == ContentStatus.VISIBLE) {
+        ScrollToFirstItemFab(
+            modifier = modifier.fillMaxSize(),
+            isFabVisible = scrollState.canScrollBackward,
+            onClickFab = { scope.launch { scrollState.animateScrollToItem(0) } }
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(
+                    modifier = Modifier.weight(1f),
+                    state = scrollState,
+                    contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
+                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+                ) {
+                    item {
+                        AddItemAppbar(
+                            title = stringResource(R.string.choose_card),
+                            onClickBack = { interactions.onSwitchContent(CartCheckOutUiState.VisibleContent.PAYMENT) },
+                            onClickAdd = interactions::controlAddCreditVisibility
+                        )
+                    }
 
-                items(state.allCreditCards) { credit ->
-                    val isSelected = state.selectedCreditCard == credit
-                    CreditCardItem(
-                        card = credit,
-                        background = if (isSelected) MaterialTheme.colors.purple else MaterialTheme.colors.primary,
-                        onClickItem = { interactions.onSelectCredit(credit) }
-                    )
+                    items(state.allCreditCards) { credit ->
+                        val isSelected = state.selectedCreditCard == credit
+                        CreditCardItem(
+                            card = credit,
+                            background = if (isSelected) MaterialTheme.colors.purple else MaterialTheme.colors.primary,
+                            onClickItem = { interactions.onSelectCredit(credit) }
+                        )
+                    }
                 }
+                PrimaryTextButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = MaterialTheme.spacing.space16)
+                        .padding(bottom = MaterialTheme.spacing.space16),
+                    text = stringResource(R.string.check_out),
+                    isEnabled = state.selectedCreditCard != CreditUiSate(),
+                    onClick = interactions::checkOutOder
+                )
             }
-            PrimaryTextButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = MaterialTheme.spacing.space16)
-                    .padding(bottom = MaterialTheme.spacing.space16),
-                text = stringResource(R.string.check_out),
-                isEnabled = state.selectedCreditCard != CreditUiSate(),
-                onClick = interactions::checkOutOder
-            )
         }
+
+        ChooseCardAddCredit(state = state, interactions = interactions)
     }
-    ChooseCardAddCredit(state = state, interactions = interactions)
+    ContentError(
+        isVisible = state.chooseCardContentStatus == ContentStatus.FAILURE,
+        onTryAgain = interactions::checkOutOder
+    )
 }
