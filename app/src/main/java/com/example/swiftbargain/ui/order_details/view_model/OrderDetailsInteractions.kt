17c2961143ee1f293682aa0dd1f2e8435b37d688
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-21 21:23:47 +0300

    implement logic getOrderDetails in VM

--- app/src/main/java/com/example/swiftbargain/ui/order_details/view_model/OrderDetailsInteractions.kt
+++ app/src/main/java/com/example/swiftbargain/ui/order_details/view_model/OrderDetailsInteractions.kt
@@ -1,3 +1,7 @@
 package com.example.swiftbargain.ui.order_details.view_model
 
-interface OrderDetailsInteractions
+interface OrderDetailsInteractions {
+    fun onClickBack()
+    fun getOrderDetails()
+    fun onClickProduct(id: String)
+}
