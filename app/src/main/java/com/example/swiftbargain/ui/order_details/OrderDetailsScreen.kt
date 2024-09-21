7631759e5603911b3380416c3af30adb21193511
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-21 21:24:05 +0300

    design OrderDetailsScreen

--- app/src/main/java/com/example/swiftbargain/ui/order_details/OrderDetailsScreen.kt
+++ app/src/main/java/com/example/swiftbargain/ui/order_details/OrderDetailsScreen.kt
@@ -1,19 +1,30 @@
 package com.example.swiftbargain.ui.order_details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.swiftbargain.R
 import com.example.swiftbargain.ui.composable.ContentError
 import com.example.swiftbargain.ui.composable.ContentLoading
 import com.example.swiftbargain.ui.composable.ContentVisibility
+import com.example.swiftbargain.ui.composable.PrimaryAppbar
+import com.example.swiftbargain.ui.order_details.composable.OrderProducts
+import com.example.swiftbargain.ui.order_details.composable.OrderStatus
+import com.example.swiftbargain.ui.order_details.composable.PaymentDetails
+import com.example.swiftbargain.ui.order_details.composable.ShippingDetails
 import com.example.swiftbargain.ui.order_details.view_model.OrderDetailsInteractions
 import com.example.swiftbargain.ui.order_details.view_model.OrderDetailsUiState
 import com.example.swiftbargain.ui.order_details.view_model.OrderDetailsViewModel
+import com.example.swiftbargain.ui.theme.spacing
 import com.example.swiftbargain.ui.utils.ContentStatus
 import com.example.swiftbargain.ui.utils.EventHandler
 
@@ -37,12 +48,36 @@ private fun OrderDetailsContent(
 ) {
     ContentLoading(isVisible = state.contentStatus == ContentStatus.LOADING)
     ContentVisibility(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        ) {
+            item {
+                PrimaryAppbar(
+                    title = stringResource(R.string.order_details),
+                    onClickBack = interactions::onClickBack
+                )
+            }
+            item {
+                OrderStatus(status = state.order.status)
+            }
+            item {
+                OrderProducts(
+                    products = state.products,
+                    onClick = interactions::onClickProduct
+                )
+            }
+            item {
+                ShippingDetails(order = state.order, products = state.products)
+            }
+            item {
+                PaymentDetails(order = state.order)
+            }
         }
     }
     ContentError(
         isVisible = state.contentStatus == ContentStatus.FAILURE,
-        onTryAgain = {}
+        onTryAgain = interactions::getOrderDetails
     )
 }
