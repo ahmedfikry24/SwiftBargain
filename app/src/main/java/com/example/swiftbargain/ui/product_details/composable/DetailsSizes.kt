fc7a7221039fe79434b2bb2ebd167b4556df9742
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-09-23 17:36:24 +0300

    handle ripple click orderDetails

--- app/src/main/java/com/example/swiftbargain/ui/product_details/composable/DetailsSizes.kt
+++ app/src/main/java/com/example/swiftbargain/ui/product_details/composable/DetailsSizes.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.swiftbargain.R
@@ -55,6 +56,7 @@ fun DetailsSizes(
                                 if (state.selectedSize == size) MaterialTheme.colors.primary else MaterialTheme.colors.textLight,
                                 CircleShape
                             )
+                            .clip(CircleShape)
                             .clickable { onClickSize(size) }
                     )
                     Text(
