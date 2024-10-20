c00bfafc73f884a099cfbb101e2c707be23dfbdb
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-10-20 23:35:50 +0300

    update NoItemFound with textButton

--- app/src/main/java/com/example/swiftbargain/ui/composable/NoItemFound.kt
+++ app/src/main/java/com/example/swiftbargain/ui/composable/NoItemFound.kt
@@ -8,32 +8,53 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.swiftbargain.R
+import com.example.swiftbargain.ui.theme.spacing
 
 @Composable
 fun NoItemFound(
     modifier: Modifier = Modifier,
     isVisible: Boolean,
+    isButtonVisible: Boolean = true,
+    buttonText: String = stringResource(R.string.add),
     enterTransition: EnterTransition = fadeIn(tween(500)) + scaleIn(tween(500)),
-    exitTransition: ExitTransition = fadeOut(tween(500)) + scaleOut(tween(500))
+    exitTransition: ExitTransition = fadeOut(tween(500)) + scaleOut(tween(500)),
+    onClickAdd: () -> Unit = {}
 ) {
+    val rawComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_no_item_found))
     AnimatedVisibility(
-        modifier = modifier,
+        modifier = modifier.fillMaxWidth(),
         visible = isVisible,
         enter = enterTransition,
         exit = exitTransition,
     ) {
-        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_no_item_found))
-        LottieAnimation(
-            modifier = Modifier.fillMaxSize(),
-            composition = composition
-        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LottieAnimation(
+                modifier = Modifier.fillMaxSize(),
+                composition = rawComposition
+            )
+            if (isButtonVisible)
+                PrimaryTextButton(
+                    text = buttonText,
+                    onClick = onClickAdd
+                )
+        }
     }
 }
